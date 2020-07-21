503b5a96d413d2c7f56faeabee0de9a503b4e251
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-07-21 22:35:30 +0530

    using full path for tests

--- test.js
+++ test.js
@@ -11,9 +11,10 @@ Post list example:
 Test content
 `;
 fs.writeFile(path.join(__dirname, 'test', 'Readme.md'), template, ()=> {
+    console.log("Written test file....");
     process.env.INPUT_MAX_POST_COUNT = "5";
     process.env.INPUT_FEED_LIST = "http://localhost:8080";
-    process.env.INPUT_README_PATH = "./test/README.md";
+    process.env.INPUT_README_PATH = path.join(__dirname, 'test', 'Readme.md');
     process.env.TEST_MODE = "true";
     require('./blog-post-workflow');
 });
