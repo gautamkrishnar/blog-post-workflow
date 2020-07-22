7f137b16b731db8c95c3fc92a71ce94e54ef1b90
Lucas Santos <lhs.santoss@gmail.com>
2020-07-22 17:36:03 -0300

    Add test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const process = require('process');
-const path = require('path');
-const fs = require('fs');
+const process = require('process')
+const path = require('path')
+const fs = require('fs')
 // language=markdown
 const template = `# Readme test
 Post list example:
@@ -9,13 +9,13 @@ Post list example:
 
 # Other contents
 Test content
-`;
-fs.writeFile(path.join(__dirname, 'test', 'Readme.md'), template, ()=> {
-    console.log("Written test file....");
-    process.env.INPUT_MAX_POST_COUNT = "5";
-    process.env.INPUT_FEED_LIST = "http://localhost:8080";
-    process.env.INPUT_README_PATH = path.join(__dirname, 'test', 'Readme.md');
-    process.env.TEST_MODE = "true";
-    require('./blog-post-workflow');
-});
+`
+fs.writeFile(path.join(__dirname, 'test', 'Readme.md'), template, () => {
+    console.log("Written test file....")
+    process.env.INPUT_MAX_POST_COUNT = "5"
+    process.env.INPUT_FEED_LIST = "http://localhost:8080"
+    process.env.INPUT_README_PATH = path.join(__dirname, 'test', 'Readme.md')
+    process.env.TEST_MODE = "true"
+    require('./blog-post-workflow')
+})
 
