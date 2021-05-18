ee0088ecfe62679597ce0fbe871be441d5f431f5
gautamkrishnar <r.gautamkrishna@gmail.com>
2021-05-19 02:44:24 +0530

    refactored test server

--- test/test-server.js
+++ test/test-server.js
@@ -4,16 +4,20 @@ const path = require('path');
 let failCounter = 0;
 const MAX_FAIL_COUNT = 5;
 
+const sendResponse = (res, statusCode, data) => {
+  res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+  res.write(data);
+  res.end();
+};
+
 const xmlData = fs.readFileSync(path.join(__dirname, 'sample.xml'), 'utf-8');
+
 http.createServer(function (req, res) {
-  if (req.url === '/failtest' && failCounter < MAX_FAIL_COUNT) {
+  if (req.url === '/failtest') {
+    const showError = failCounter !== MAX_FAIL_COUNT;
+    sendResponse(res, showError ? 500 : 200, showError ? 'error!' : xmlData);
     failCounter = failCounter + 1;
-    res.writeHead(500, {'Content-Type': 'text/plain'});
-    res.write('error!');
-    res.end();
   } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(xmlData);
-    res.end();
+    sendResponse(res, 200, xmlData);
   }
 }).listen(8080);
